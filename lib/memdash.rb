a1797a868ac262747706d6e7772d890b1b51117c
Steve Richert <steve.richert@gmail.com>
2012-04-06 14:55:21 -0400

    IN FLUX

--- lib/memdash.rb
+++ lib/memdash.rb
@@ -1,2 +1,36 @@
+require "dalli"
+
 module Memdash
+  def self.included(base)
+    base.class_eval do
+      alias_method :perform_without_stats, :perform
+      alias_method :perform, :perform_with_stats
+    end
+  end
+
+  def perform_with_stats(op, key, *args)
+    result = perform_without_stats(op, key, *args)
+    generate_stats(op, key, *args)
+    result
+  end
+
+  def generate_stats(op, key, *args)
+    val = perform_without_stats(:get, "memdash")
+    if val.nil?
+      perform_without_stats(:add, "memdash", "BAM", 3, {})
+    end
+
+    puts [op, key, args].inspect
+  end
+
+  def fetch_without_stats(key, value, ttl)
+    val = perform_without_stats(:get, key)
+    if val.nil? && block_given?
+      val = value
+      add(key, val, ttl)
+    end
+    val
+  end
 end
+
+Dalli::Client.send(:include, Memdash)
