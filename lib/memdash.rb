100896c74d55b28793c0c762db33546631970862
Brian Ryckbost <bryckbost@gmail.com>
2012-04-07 08:54:39 -0400

    Allow for a configurable memdash ttl

--- lib/memdash.rb
+++ lib/memdash.rb
@@ -1,8 +1,15 @@
 require "dalli"
 
 module Memdash
+  DEFAULT_TTL = 60
+
   def self.included(base)
     base.class_eval do
+      class << base
+        attr_accessor :memdash_ttl
+      end
+      self.memdash_ttl = DEFAULT_TTL
+
       alias_method :perform_without_stats, :perform
       alias_method :perform, :perform_with_stats
     end
@@ -17,7 +24,7 @@ module Memdash
   def generate_stats(op, key, *args)
     val = perform_without_stats(:get, "memdash")
     if val.nil?
-      perform_without_stats(:add, "memdash", "BAM", 3, {})
+      perform_without_stats(:add, "memdash", "BAM", self.class.memdash_ttl, {})
     end
 
     puts [op, key, args].inspect
